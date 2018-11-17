733de65887eb4f3b708d873e30654d011ea37e25
CptZeru <zerulegend@gmail.com>
2018-11-17 21:57:40 +0700

    fix timestamps issues

--- app/obat.php
+++ app/obat.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class obat extends Model
 {
     protected $fillable = ['nama_obat', 'harga', 'stok', 'expired_date', 'production_date'];
+    public $timestamps = false;
 }
